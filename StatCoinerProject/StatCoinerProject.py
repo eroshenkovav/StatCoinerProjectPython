e96e18819928384a079c457323ae984d7d3443fc
Artem Eroshenkov <eroshenkovav@gmail.com>
2018-03-23 15:33:36 +0300

    23.03.2018 - Init Commit

--- StatCoinerProject/StatCoinerProject.py
+++ StatCoinerProject/StatCoinerProject.py
@@ -1,1 +1,22 @@
+"""
+Author: Eroshenkov Artem
+Date: 23.03.2018
+"""
+from coincap import CoinCap
+from time import gmtime, strftime
 
+def main():
+    my_instance = CoinCap()
+    timestamp = strftime("%Y_%m_%d %H_%M_%S", gmtime())
+    
+    file = open("coincapio_front_" + timestamp + ".txt", "w")
+    
+    front_list = my_instance.get_front()
+    for front_item in front_list:
+        for front_item_detail in front_item:
+            file.write(front_item_detail + ": " + str(front_item.get(front_item_detail)) + "\n") 
+    
+    file.close()
+
+if __name__ == "__main__":
+    main()
